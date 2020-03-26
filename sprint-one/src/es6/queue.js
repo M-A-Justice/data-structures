6744d30ffa8f1723b6722fe32a96e0f714bb99b9
Michael Justice <m_a_justice@yahoo.com>
2020-03-26 11:26:14 -0700

    completed all queue instantiation styles

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,27 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, oldestCount, newestCount) {
+    this.storage = {};
+    this.newestCount = 0;
+    this.oldestCount = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.newestCount] = value;
+    this.newestCount++;
+  }
+  dequeue() {
+    var dequeued = this.storage[this.oldestCount];
+    delete this.storage[this.oldestCount];
+    this.oldestCount++;
+    return dequeued;
+  }
+  size() {
+    var size = this.newestCount - this.oldestCount;
+    if (size < 0) {
+      size = 0;
+    }
+    return size;
+  }
 }
