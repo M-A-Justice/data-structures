217dc55fac6e72ca891b95c80df8f7e7d55d0361
Michael Justice <m_a_justice@yahoo.com>
2020-03-27 15:16:01 -0700

    completed linkedList w/ Jim Knape

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -20,16 +20,30 @@ var LinkedList = function() {
   };
 
   list.removeHead = function() {
+    // store former head
+    var formerHead = list.head;
     // move head to next node
+    list.head = list.head.next;
+    // return former head value
+    return formerHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
     // while loop starting at the head
-    // if target equals current value
-    // return true
-    // otherwise
-    // set current node to equal next node
+    while (currentNode) {
+      // if target equals current value
+      if (target === currentNode.value) {
+        // return true
+        return true;
+        // otherwise
+      } else {
+        // set current node to equal next node
+        currentNode = currentNode.next;
+      }
+    }
     // if not found while loop will exit and return false
+    return false;
   };
 
   return list;
