80775d4b1a3d8faf399694360deb4bdfffd8b396
Michael Justice <m_a_justice@yahoo.com>
2020-03-27 14:56:33 -0700

    added tail addition to linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    // if no nodes
+    if (!list.head) {
+      // head and tail = node
+      list.head = node;
+      list.tail = node;
+      // otherwise
+    } else {
+      // add node to the tail
+      list.tail.next = node;
+      // and set tail to equal node
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    // move head to next node
   };
 
   list.contains = function(target) {
+    // while loop starting at the head
+    // if target equals current value
+    // return true
+    // otherwise
+    // set current node to equal next node
+    // if not found while loop will exit and return false
   };
 
   return list;
