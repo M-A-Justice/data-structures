47bfc0f8c85bf71d460bf785bb92a7cc1f2f4067
Michael Justice <m_a_justice@yahoo.com>
2020-03-28 18:49:33 -0700

    completed hashTable without resize functions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,45 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = [];
+  // if storage bucket does not contain tuple
+  if (Array.isArray(this._storage[index])) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      var tuple = this._storage[index][i];
+      if (tuple[0] === k) {
+        tuple[1] = v;
+      } else {
+        this._storage[index].push([k, v]);
+      }
+    }
+  } else {
+    // add key/value tuple
+    bucket.push([k, v]);
+    // storage
+    this._storage[index] = bucket;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  var result;
+  bucket.forEach(function(tuple) {
+    if (tuple === undefined) {
+      return undefined;
+    }
+    if (tuple[0] === k) {
+      result = tuple[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    this._storage[index][i] = undefined;
+  }
 };
 
 
